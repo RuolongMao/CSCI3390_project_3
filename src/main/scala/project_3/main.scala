c15d695a7b39765b4e8d0f23c66c2b3cb85b0347
unknown <marcomao0719@gmail.com>
2025-04-09 14:43:26 -0400

    implementations

--- src/main/scala/project_3/main.scala
+++ src/main/scala/project_3/main.scala
@@ -17,14 +17,68 @@ object main{
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
   def LubyMIS(g_in: Graph[Int, Int]): Graph[Int, Int] = {
-    while (remaining_vertices >= 1) {
-        // To Implement
+    var g = g_in.mapVertices((_, _) => 0)
+    var remainingVertices = g.vertices.filter { case (_, attr) => attr == 0 }.count()
+    var iteration = 0
+
+    while (remainingVertices >= 1) {
+      iteration += 1
+      val randomGraph = g.mapVertices((id, attr) => {
+        if (attr == 0) scala.util.Random.nextDouble() else attr
+      })
+
+      val localMinima = randomGraph.aggregateMessages[Double](
+        triplet => {
+          if (triplet.srcAttr > 0 && triplet.dstAttr > 0) {
+            if (triplet.srcAttr < triplet.dstAttr) triplet.sendToDst(triplet.srcAttr)
+            else triplet.sendToSrc(triplet.dstAttr)
+          }
+        },
+        (a, b) => math.min(a, b)
+      )
+
+      val newGraph = g.outerJoinVertices(localMinima) { (vid, oldAttr, minNeighbor) =>
+        if (oldAttr == 0) {
+          if (minNeighbor.isEmpty) 1
+          else -1
+        } else oldAttr
+      }
+
+      g = newGraph
+      remainingVertices = g.vertices.filter { case (_, attr) => attr == 0 }.count()
     }
+
+    g
   }
 
 
   def verifyMIS(g_in: Graph[Int, Int]): Boolean = {
-    // To Implement
+    val misVertices = g_in.vertices.filter { case (_, attr) => attr == 1 }.map(_._1).collect().toSet
+
+    val violatingEdges = g_in.edges.filter(e => misVertices.contains(e.srcId) && misVertices.contains(e.dstId))
+    if (!violatingEdges.isEmpty()) {
+      return false
+    }
+
+    val nonMISVertices = g_in.vertices.filter { case (_, attr) => attr != 1 }
+    
+    val neighborsInMIS = g_in.aggregateMessages[Boolean](
+      triplet => {
+        if (triplet.dstAttr == 1) triplet.sendToSrc(true)
+        if (triplet.srcAttr == 1) triplet.sendToDst(true)
+      },
+      (a, b) => a || b
+    )
+
+    val maximalityViolations = nonMISVertices.leftOuterJoin(neighborsInMIS)
+      .filter { case (_, (_, hasNeighbor)) => !hasNeighbor.getOrElse(false) }
+      .count()
+
+    if (maximalityViolations > 0) {
+      return false
+    }
+
+    true
   }
 
 
